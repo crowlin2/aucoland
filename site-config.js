bf04832aa83167fd0e6e4a78f9549c15aec8dd59
crowlin2 <afgut96@gmail.com>
2026-07-04 23:37:29 -0400

    Agregar configuración comercial centralizada

---
+++ site-config.js
@@ -0,0 +1,21 @@
+(function () {
+  window.AUCO_CONFIG = Object.freeze({
+    priceUF: 118,
+    downPaymentPercent: 10,
+    installments: 72,
+    whatsappNumber: "56950103278",
+    agreementDiscountPercent: 15,
+    agreements: Object.freeze([
+      "Carabineros de Chile",
+      "Personas mayores",
+      "Municipalidad de Rinconada",
+      "Coopeuch",
+      "Andescoop"
+    ]),
+    copy: Object.freeze({
+      heroTitle: "Planifica una sepultura familiar con tranquilidad",
+      heroLead: "Alternativas familiares perpetuas desde 118 UF, con pie desde el 10% y hasta 72 cuotas.",
+      heroSecondary: "Recibe orientación personalizada y compara ubicaciones antes de decidir."
+    })
+  });
+})();
